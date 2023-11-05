33f9b392d2a6f84e2690709165880754f8ccf439
Candace Hendricks <candiiihendricks@gmail.com>
2023-11-05 14:47:47 -0500

    Added edits to the css and javascript files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+
+
+ const btn = document.querySelector("button");
+ btn.addEventListener("click", function(event){
+ event.preventDefault();
+ const email = document.getElementById("email").value;
+ const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (email == ""){
+    document.querySelector(".message").textContent = "Please enter a valid email address";
+  }else if (!emailPattern.test(email)) {
+    document.querySelector(".message").textContent = "Please enter a valid email address";
+  } else {
+    document.querySelector(".message").textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+  }
+
+ console.log(email);
+}); 
+
+
+
+}
